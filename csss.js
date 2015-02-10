00e06ddeafe57ae05c9c74030b00413587eb5fd4
webycel <anthony.web91@gmail.com>
2015-02-10 10:48:29 +0000

    added info that merging option is WIP

--- csss.js
+++ csss.js
@@ -433,7 +433,7 @@ var csss = {
 program.version(pkg.version);
 
 program
-	.option('-m, --merge <newFileName>', 'merge all duplicate selectors into new file');
+	.option('-m, --merge <newFileName>', 'merge all duplicate selectors into new file (still WIP!)');
 
 program
 	.usage('[options]')

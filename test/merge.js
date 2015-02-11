d7e08e6565cc43152a2f6e0cffeb283597a95799
webycel <anthony.web91@gmail.com>
2015-02-11 09:47:21 +0000

    moved comments for better understanding

--- test/merge.js
+++ test/merge.js
@@ -12,14 +12,14 @@ var fs = require('fs'),
 
 describe('Merge CSS', function () {
 
-	//get CSS from test input file
-	//parse it
-	//search for duplicates
-	//merge selectors
 	beforeEach(function () {
+		//get CSS from test input file
 		return csss.getCSSFiles(testFiles).then(function (rawCSS) {
+			//parse it
 			var parsedCSS = csss.parseCss(rawCSS, testFiles);
+			//search for duplicates
 			csss.detectDuplicateSelectors(parsedCSS).then(function (r) {
+				//merge selectors
 				csss.mergeCSS(r[0][0], r[0][1], r[0][2]).then(function (m) {
 					mergedCSS = css.stringify(m[0]);
 				});
@@ -27,8 +27,8 @@ describe('Merge CSS', function () {
 		});
 	});
 
-	//get CSS from result file for comparing
 	beforeEach(function () {
+		//get CSS from result file for comparing
 		return csss.getCSSFiles(['./test/merge-results.css']).then(function (rawCSS) {
 			testCSS = css.stringify(csss.parseCss(rawCSS, testFiles));
 		});
